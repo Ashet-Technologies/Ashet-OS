8450f21fd885d63153e23d059b17f3bfa639283c
Felix "xq" Queißner <xq@random-projects.net>
2022-11-19 17:34:11 +0100

    Adds EventId demo

--- src/apps/gui-demo.zig
+++ src/apps/gui-demo.zig
@@ -6,16 +6,28 @@ const ColorIndex = ashet.abi.ColorIndex;
 
 pub usingnamespace ashet.core;
 
+const events = struct {
+    pub const cancel = @intToEnum(gui.EventID, 1);
+    pub const login = @intToEnum(gui.EventID, 2);
+};
+
 var interface = gui.Interface{ .widgets = &widgets };
-var widgets = [_]gui.Widget{
-    gui.Panel.new(5, 5, 172, 57),
-    gui.Panel.new(5, 65, 172, 57),
-    gui.Button.new(69, 42, null, "Cancel"),
-    gui.Button.new(135, 42, null, "Login"),
-    gui.TextBox.new(69, 14, 99, "xq"),
-    gui.TextBox.new(69, 28, 99, "********"),
-    gui.Label.new(15, 16, "Username:"),
-    gui.Label.new(15, 30, "Password:"),
+var widgets = blk: {
+    var list = [_]gui.Widget{
+        gui.Panel.new(5, 5, 172, 57),
+        gui.Panel.new(5, 65, 172, 57),
+        gui.Button.new(69, 42, null, "Cancel"),
+        gui.Button.new(135, 42, null, "Login"),
+        gui.TextBox.new(69, 14, 99, "xq"),
+        gui.TextBox.new(69, 28, 99, "********"),
+        gui.Label.new(15, 16, "Username:"),
+        gui.Label.new(15, 30, "Password:"),
+    };
+
+    list[2].control.button.clickEvent = gui.Event.new(events.cancel);
+    list[3].control.button.clickEvent = gui.Event.new(events.login);
+
+    break :blk list;
 };
 
 pub fn main() !void {
@@ -51,6 +63,12 @@ pub fn main() !void {
 
 fn handleEvent(evt: gui.Event) void {
     switch (evt.id) {
+        events.cancel => {
+            std.log.err("cancel was clicked", .{});
+        },
+        events.login => {
+            std.log.err("login was clicked", .{});
+        },
         else => std.log.info("unhandled gui event: {}\n", .{evt}),
     }
 }
