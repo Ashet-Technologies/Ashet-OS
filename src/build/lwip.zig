37c2c81b02621fe0e755c40b4db1b137d44f33bb
Krzysztof Wolicki <der.teufel.mail@gmail.com>
2024-06-19 22:43:52 +0200

    lwip links libc

--- src/build/lwip.zig
+++ src/build/lwip.zig
@@ -106,6 +106,7 @@ pub fn create(b: *std.Build, target: std.Build.ResolvedTarget, optimize: std.bui
         .name = "lwip",
         .target = target,
         .optimize = optimize,
+        .link_libc = true,
     });
 
     const upstream = b.dependency("lwip", .{});
