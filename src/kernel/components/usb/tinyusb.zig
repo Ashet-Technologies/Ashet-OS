367c9c7dfacadfdb80555c767257399af58d94c9
Felix Queißner <git@random-projects.net>
2025-02-14 21:41:29 +0100

    Makes TinyUSB linker errors be gone. Will panic if ever used like this.

---
+++ src/kernel/components/usb/tinyusb.zig
@@ -0,0 +1,62 @@
+const std = @import("std");
+const ashet = @import("../../main.zig");
+
+export fn hcd_device_close() void {
+    @panic("hcd_device_close not implemented yet!");
+}
+
+export fn hcd_edpt_abort_xfer() void {
+    @panic("hcd_edpt_abort_xfer not implemented yet!");
+}
+
+export fn hcd_edpt_open() void {
+    @panic("hcd_edpt_open not implemented yet!");
+}
+
+export fn hcd_edpt_xfer() void {
+    @panic("hcd_edpt_xfer not implemented yet!");
+}
+
+export fn hcd_init() void {
+    @panic("hcd_init not implemented yet!");
+}
+
+export fn hcd_int_disable() void {
+    @panic("hcd_int_disable not implemented yet!");
+}
+
+export fn hcd_int_enable() void {
+    @panic("hcd_int_enable not implemented yet!");
+}
+
+export fn hcd_int_handler() void {
+    @panic("hcd_int_handler not implemented yet!");
+}
+
+export fn hcd_port_connect_status() void {
+    @panic("hcd_port_connect_status not implemented yet!");
+}
+
+export fn hcd_port_reset() void {
+    @panic("hcd_port_reset not implemented yet!");
+}
+
+export fn hcd_port_reset_end() void {
+    @panic("hcd_port_reset_end not implemented yet!");
+}
+
+export fn hcd_port_speed_get() void {
+    @panic("hcd_port_speed_get not implemented yet!");
+}
+
+export fn hcd_setup_send() void {
+    @panic("hcd_setup_send not implemented yet!");
+}
+
+export fn tuh_hid_report_received_cb() void {
+    @panic("tuh_hid_report_received_cb not implemented yet!");
+}
+
+export fn tusb_time_millis_api() void {
+    @panic("tusb_time_millis_api not implemented yet!");
+}
