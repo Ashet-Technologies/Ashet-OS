c2528788632a7eef613cb740665f6384c25a1b1d
Felix "xq" Queißner <git@random-projects.net>
2026-02-02 20:36:34 +0100

    Vibecoded: Fixes X11 implementation

--- src/kernel/port/machine/x86/hosted-linux/hosted-linux.zig
+++ src/kernel/port/machine/x86/hosted-linux/hosted-linux.zig
@@ -47,10 +47,9 @@ fn initialize() !void {
 
 const video_drivers: std.StaticStringMap(hosted.VideoDriverCtor) = .initComptime(.{
     // TODO: Re-enable hosted video drivers
-    // .{ "x11", video_drivers_ctors.x11 },
+    .{ "x11", video_drivers_ctors.x11 },
     .{ "wayland", video_drivers_ctors.wayland },
-    .{ "auto-window", video_drivers_ctors.wayland },
-    // .{ "auto-window", video_drivers_ctors.auto },
+    .{ "auto-window", video_drivers_ctors.auto },
 });
 
 const video_drivers_ctors = struct {
