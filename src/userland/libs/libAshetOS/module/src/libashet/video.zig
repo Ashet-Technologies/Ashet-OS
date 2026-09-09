3eb35c15fa00fd2b5ea72726a6aaa1cc34471a5d
Felix "xq" Queißner <git@random-projects.net>
2026-09-09 23:19:42 +0200

    Makes OS compile again

--- src/userland/libs/libAshetOS/module/src/libashet/video.zig
+++ src/userland/libs/libAshetOS/module/src/libashet/video.zig
@@ -4,23 +4,28 @@ const ashet = @import("../libashet.zig");
 
 const abi = ashet.abi;
 
+pub const VideoOutputID = abi.video.VideoOutputID;
+pub const VideoMemory = abi.video.VideoMemory;
+pub const BufferKind = abi.video.BufferKind;
+pub const PresentMode = abi.video.PresentMode;
+
 pub const WaitForVBlank = ashet.abi.video.WaitForVBlank;
 
 pub const Output = opaque {
     pub fn release(out: *Output) void {
-        _ = out;
+        abi.resources.release(.from_ptr(out));
     }
 
     pub fn get_resolution(out: *Output) !abi.Size {
         return try abi.video.get_resolution(@ptrCast(out));
     }
 
-    pub fn get_video_memory(out: *Output) !abi.VideoMemory {
-        return try abi.video.get_video_memory(@ptrCast(out));
-    }
+    // pub fn get_video_memory(out: *Output) !abi.VideoMemory {
+    //     return try abi.video.get_video_memory(@ptrCast(out));
+    // }
 };
 
-pub fn acquire(id: abi.VideoOutputID) !*Output {
+pub fn acquire(id: VideoOutputID) !*Output {
     return @ptrCast(
         try abi.video.acquire(id),
     );
